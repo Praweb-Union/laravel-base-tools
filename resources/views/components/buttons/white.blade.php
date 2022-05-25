679f4f7f9821001ba8d5ee9a54e5f78452a1d4e4
Gromfer71 <usdenis70@gmail.com>
2022-05-25 15:00:20 +0500

    Новые компоненты для фронта

---
+++ resources/views/components/buttons/white.blade.php
@@ -0,0 +1,3 @@
+<button {{ $attributes }} type="button" class="inline-block px-6 py-2.5 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out">
+    {{ $slot }}
+</button>
---
+++ resources/views/components/offcanvas/right.blade.php
@@ -0,0 +1,13 @@
+@props(['id' => 'modal', 'title', 'isModalOpened' => false])
+<div class="offcanvas offcanvas-end fixed bottom-0 flex flex-col max-w-full bg-white  bg-clip-padding shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 top-0 right-0 border-none w-96 @if($isModalOpened) show @endif" id="{{ $id }}">
+    <div class="offcanvas-header flex items-center justify-between p-4">
+        <h5 class="offcanvas-title mb-0 leading-normal font-semibold">{{ $title }}</h5>
+        <button type="button" class="btn-close box-content w-4 h-4 p-2 -my-5 -mr-2 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" wire:click="closeAllModals()"></button>
+    </div>
+    <div class="offcanvas-body flex-grow p-4 overflow-y-auto">
+        {{ $slot }}
+    </div>
+</div>
+@if($isModalOpened)
+    <div class="offcanvas-backdrop fade show" wire:click="closeAllModals()"></div>
+@endif
