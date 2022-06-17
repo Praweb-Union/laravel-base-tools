c7080e7a12a411fbc8f307ce095100246c410512
Gromfer71 <usdenis70@gmail.com>
2022-06-17 14:03:39 +0500

    Правки

---
+++ resources/views/components/input/html-editor/init.blade.php
@@ -0,0 +1,7 @@
+<script>
+    tinymce.init({
+        selector: 'textarea', // Replace this CSS selector to match the placeholder element for TinyMCE
+        plugins: 'code table lists',
+        toolbar: 'undo redo | formatselect| bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+    });
+</script>
