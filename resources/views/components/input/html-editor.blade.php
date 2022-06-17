601df1691ce01d5c3666148dc61167ba1c12f66a
Gromfer71 <usdenis70@gmail.com>
2022-06-17 15:01:31 +0500

    Html editor

--- resources/views/components/input/html-editor.blade.php
+++ resources/views/components/input/html-editor.blade.php
@@ -4,10 +4,3 @@
           placeholder="{{ $placeholder }}">
     {{ $value }}
 </textarea>
-<script>
-    tinymce.init({
-        selector: 'textarea#html-editor', // Replace this CSS selector to match the placeholder element for TinyMCE
-        plugins: 'code table lists',
-        toolbar: 'undo redo | formatselect| bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
-    });
-</script>
