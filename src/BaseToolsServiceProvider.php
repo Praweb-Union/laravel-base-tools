8a678de020ab78ca4622e4995237b9e244fe52af
Gromfer71 <usdenis70@gmail.com>
2022-06-23 12:17:45 +0500

    Регистрация компонентов

--- src/BaseToolsServiceProvider.php
+++ src/BaseToolsServiceProvider.php
@@ -8,10 +8,10 @@ class BaseToolsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'praweb');
         $this->publishes([
-                             __DIR__ . '/../resources/views/components' => resource_path('views/components'),
-                             __DIR__ . '/../resources/views/livewire' => resource_path('views/livewire'),
-
-                         ], 'base-tools-components');
+            __DIR__ . '/../resources/views/components' => resource_path('views/vendor/praweb/components'),
+            __DIR__ . '/../resources/views/livewire' => resource_path('views/vendor/praweb/livewire'),
+        ], 'base-tools-components');
     }
 }
