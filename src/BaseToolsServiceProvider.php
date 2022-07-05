5d79d878fb75cce456886823e419d122b51409ef
Gromfer71 <usdenis70@gmail.com>
2022-07-05 14:43:35 +0500

    Регистрация компонента уведомлений

--- src/BaseToolsServiceProvider.php
+++ src/BaseToolsServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Praweb\BaseTools;
 
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class BaseToolsServiceProvider extends ServiceProvider
 {
@@ -13,5 +14,6 @@ class BaseToolsServiceProvider extends ServiceProvider
             __DIR__ . '/../resources/views/components' => resource_path('views/vendor/praweb/components'),
             __DIR__ . '/../resources/views/livewire' => resource_path('views/vendor/praweb/livewire'),
         ], 'base-tools-components');
+        Livewire::component('x-praweb::notifications', Notifications::class);
     }
 }
